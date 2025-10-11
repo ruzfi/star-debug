713f24ef1e3ab9cbf0bcfe533c89e1ac76eea2da
Ihor Lytvynenko <igor@ert.org.ua>
2025-10-11 12:53:39 +0300

    *proto

--- star.debug/lib/grpc/starlink/starlink.pbenum.dart
+++ star.debug/lib/grpc/starlink/starlink.pbenum.dart
@@ -464,69 +464,6 @@ class SpeedtestError extends $pb.ProtobufEnum {
   const SpeedtestError._(super.value, super.name);
 }
 
-/// SpaceX.API.Satellites.Network.UtDisablementCode is an enum:
-class UtDisablementCode extends $pb.ProtobufEnum {
-  static const UtDisablementCode UNKNOWN_STATE =
-      UtDisablementCode._(0, _omitEnumNames ? '' : 'UNKNOWN_STATE');
-  static const UtDisablementCode OKAY =
-      UtDisablementCode._(1, _omitEnumNames ? '' : 'OKAY');
-  static const UtDisablementCode NO_ACTIVE_ACCOUNT =
-      UtDisablementCode._(2, _omitEnumNames ? '' : 'NO_ACTIVE_ACCOUNT');
-  static const UtDisablementCode TOO_FAR_FROM_SERVICE_ADDRESS =
-      UtDisablementCode._(
-          3, _omitEnumNames ? '' : 'TOO_FAR_FROM_SERVICE_ADDRESS');
-  static const UtDisablementCode IN_OCEAN =
-      UtDisablementCode._(4, _omitEnumNames ? '' : 'IN_OCEAN');
-  static const UtDisablementCode BLOCKED_COUNTRY =
-      UtDisablementCode._(6, _omitEnumNames ? '' : 'BLOCKED_COUNTRY');
-  static const UtDisablementCode DATA_OVERAGE_SANDBOX_POLICY =
-      UtDisablementCode._(
-          7, _omitEnumNames ? '' : 'DATA_OVERAGE_SANDBOX_POLICY');
-  static const UtDisablementCode CELL_IS_DISABLED =
-      UtDisablementCode._(8, _omitEnumNames ? '' : 'CELL_IS_DISABLED');
-  static const UtDisablementCode ROAM_RESTRICTED =
-      UtDisablementCode._(10, _omitEnumNames ? '' : 'ROAM_RESTRICTED');
-  static const UtDisablementCode UNKNOWN_LOCATION =
-      UtDisablementCode._(11, _omitEnumNames ? '' : 'UNKNOWN_LOCATION');
-  static const UtDisablementCode ACCOUNT_DISABLED =
-      UtDisablementCode._(12, _omitEnumNames ? '' : 'ACCOUNT_DISABLED');
-  static const UtDisablementCode UNSUPPORTED_VERSION =
-      UtDisablementCode._(13, _omitEnumNames ? '' : 'UNSUPPORTED_VERSION');
-  static const UtDisablementCode MOVING_TOO_FAST_FOR_POLICY =
-      UtDisablementCode._(
-          14, _omitEnumNames ? '' : 'MOVING_TOO_FAST_FOR_POLICY');
-  static const UtDisablementCode UNDER_AVIATION_FLYOVER_LIMITS =
-      UtDisablementCode._(
-          15, _omitEnumNames ? '' : 'UNDER_AVIATION_FLYOVER_LIMITS');
-  static const UtDisablementCode BLOCKED_AREA =
-      UtDisablementCode._(16, _omitEnumNames ? '' : 'BLOCKED_AREA');
-
-  static const $core.List<UtDisablementCode> values = <UtDisablementCode>[
-    UNKNOWN_STATE,
-    OKAY,
-    NO_ACTIVE_ACCOUNT,
-    TOO_FAR_FROM_SERVICE_ADDRESS,
-    IN_OCEAN,
-    BLOCKED_COUNTRY,
-    DATA_OVERAGE_SANDBOX_POLICY,
-    CELL_IS_DISABLED,
-    ROAM_RESTRICTED,
-    UNKNOWN_LOCATION,
-    ACCOUNT_DISABLED,
-    UNSUPPORTED_VERSION,
-    MOVING_TOO_FAST_FOR_POLICY,
-    UNDER_AVIATION_FLYOVER_LIMITS,
-    BLOCKED_AREA,
-  ];
-
-  static final $core.List<UtDisablementCode?> _byValue =
-      $pb.ProtobufEnum.$_initByValueList(values, 16);
-  static UtDisablementCode? valueOf($core.int value) =>
-      value < 0 || value >= _byValue.length ? null : _byValue[value];
-
-  const UtDisablementCode._(super.value, super.name);
-}
-
 /// SpaceX.API.Device.UserMobilityClass is an enum:
 class UserMobilityClass extends $pb.ProtobufEnum {
   static const UserMobilityClass STATIONARY =
--- star.debug/lib/grpc/starlink/starlink.pbjson.dart
+++ star.debug/lib/grpc/starlink/starlink.pbjson.dart
@@ -282,44 +282,6 @@ final $typed_data.Uint8List speedtestErrorDescriptor = $convert.base64Decode(
     'X0VSUk9SX0FQSRADEh0KGVNQRUVEVEVTVF9FUlJPUl9OT19SRVNVTFQQBBIbChdTUEVFRFRFU1'
     'RfRVJST1JfT0ZGTElORRAF');
 
-@$core.Deprecated('Use utDisablementCodeDescriptor instead')
-const UtDisablementCode$json = {
-  '1': 'UtDisablementCode',
-  '2': [
-    {'1': 'UNKNOWN_STATE', '2': 0},
-    {'1': 'OKAY', '2': 1},
-    {'1': 'NO_ACTIVE_ACCOUNT', '2': 2},
-    {'1': 'TOO_FAR_FROM_SERVICE_ADDRESS', '2': 3},
-    {'1': 'IN_OCEAN', '2': 4},
-    {'1': 'BLOCKED_COUNTRY', '2': 6},
-    {'1': 'DATA_OVERAGE_SANDBOX_POLICY', '2': 7},
-    {'1': 'CELL_IS_DISABLED', '2': 8},
-    {'1': 'ROAM_RESTRICTED', '2': 10},
-    {'1': 'UNKNOWN_LOCATION', '2': 11},
-    {'1': 'ACCOUNT_DISABLED', '2': 12},
-    {'1': 'UNSUPPORTED_VERSION', '2': 13},
-    {'1': 'MOVING_TOO_FAST_FOR_POLICY', '2': 14},
-    {'1': 'UNDER_AVIATION_FLYOVER_LIMITS', '2': 15},
-    {'1': 'BLOCKED_AREA', '2': 16},
-  ],
-  '4': [
-    {'1': 5, '2': 5},
-    {'1': 9, '2': 9},
-  ],
-  '5': ['INVALID_COUNTRY', 'UNLICENSED_COUNTRY'],
-};
-
-/// Descriptor for `UtDisablementCode`. Decode as a `google.protobuf.EnumDescriptorProto`.
-final $typed_data.Uint8List utDisablementCodeDescriptor = $convert.base64Decode(
-    'ChFVdERpc2FibGVtZW50Q29kZRIRCg1VTktOT1dOX1NUQVRFEAASCAoET0tBWRABEhUKEU5PX0'
-    'FDVElWRV9BQ0NPVU5UEAISIAocVE9PX0ZBUl9GUk9NX1NFUlZJQ0VfQUREUkVTUxADEgwKCElO'
-    'X09DRUFOEAQSEwoPQkxPQ0tFRF9DT1VOVFJZEAYSHwobREFUQV9PVkVSQUdFX1NBTkRCT1hfUE'
-    '9MSUNZEAcSFAoQQ0VMTF9JU19ESVNBQkxFRBAIEhMKD1JPQU1fUkVTVFJJQ1RFRBAKEhQKEFVO'
-    'S05PV05fTE9DQVRJT04QCxIUChBBQ0NPVU5UX0RJU0FCTEVEEAwSFwoTVU5TVVBQT1JURURfVk'
-    'VSU0lPThANEh4KGk1PVklOR19UT09fRkFTVF9GT1JfUE9MSUNZEA4SIQodVU5ERVJfQVZJQVRJ'
-    'T05fRkxZT1ZFUl9MSU1JVFMQDxIQCgxCTE9DS0VEX0FSRUEQECIECAUQBSIECAkQCSoPSU5WQU'
-    'xJRF9DT1VOVFJZKhJVTkxJQ0VOU0VEX0NPVU5UUlk=');
-
 @$core.Deprecated('Use userMobilityClassDescriptor instead')
 const UserMobilityClass$json = {
   '1': 'UserMobilityClass',
